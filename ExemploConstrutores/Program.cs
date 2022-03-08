8c5f99c1906e2abc9982b30fee1fe9ba69b2cbab
pedroasso <pedroasso@hotmail.com>
2022-03-08 08:09:04 -0300

    inicio da segunda aula C# - Construtores

---
+++ ExemploConstrutores/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ExemploConstrutores.Models;
+
+namespace ExemploConstrutores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log log = Log.GetInstance();
+
+            // Pessoa p1 = new Pessoa();
+            // p1.Apresentar();
+        }
+    }
+}
