cb4a298385b2d7e3aaa2bad3f881fb3a75f19e51
pedroasso <pedroasso@hotmail.com>
2022-03-09 21:33:52 -0300

    finalizando aula de construtores

--- ExemploConstrutores/Program.cs
+++ ExemploConstrutores/Program.cs
@@ -5,11 +5,31 @@ namespace ExemploConstrutores
 {
     class Program
     {
+        public delegate void Operacao(int x, int y);
         static void Main(string[] args)
         {
-            Log log = Log.GetInstance();
+            Matematica m = new Matematica(10, 20);
+            m.Somar();
+            
+            // //Operacao op = Calculadora.Somar;
+            // Operacao op = new Operacao(Calculadora.Somar);
+            // op += Calculadora.Subtrair;
 
-            // Pessoa p1 = new Pessoa();
+            // //op.Invoke(10, 10);
+            // op(10, 10);
+
+            // const double pi = 3.14;
+            // System.Console.WriteLine(pi);
+        
+            // Data data = new Data();
+            // //data.SetMes(13);
+
+            // data.Mes = 20;
+            // System.Console.WriteLine(data.Mes);
+
+            // data.ApresentarMes();
+
+            // Aluno p1 = new Aluno("Pedro", "Laranjeira", "Teste");
             // p1.Apresentar();
         }
     }
